6bb99e12d01c5058ec122d4899fcaf11a8524077
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-23 14:07:46 -1000

    GET fixed, /reset /heard addded, error handling added

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
 const jsonParser = bodyParser.json();
 const urlParser = bodyParser.urlencoded({ extended: false });
 //const about = require('./routes/about.js');
 const PORT = 3000;
 
 let buzzwords = [];
+let score = 0;
 
 app.use(express.static('public'));
-app.use(urlParser);
+//app.use(urlParser);
 
 app.get('/', (req, res) => {
   res.render('index.html');
 });
 
 app.get('/buzzwords', (req, res) => {
-  console.log(buzzwords);
-  res.send(`${buzzwords}`);
+  res.send(`"buzzwords": ${JSON.stringify(buzzwords)}`);
 });
 
 app.post('/buzzwords', urlParser, (req, res) => {
-  if (buzzwords.length <= 5) {
+  //check for 'buzzword' & 'points' keys
+  const hasKeys = !(req.body.buzzword === undefined || req.body.points === undefined);
+  if ((buzzwords.length <= 5) & hasKeys) {
     buzzwords.push(req.body);
+    res.send(`{ "success": true}`);
+  } else if (buzzwords.length > 5 || !hasKeys) {
+    res.send(`{ "success": false}`);
   }
-  res.send(`{ "success": true}`);
-  console.log(buzzwords);
 });
 
 app.put('/buzzwords', urlParser, (req, res) => {
   const findBuzzword = buzzwords.indexOf(buzzwords.find((obj) => obj.buzzword === req.body.buzzword));
-  if (findBuzzword > -1) {
+  const hasKeys = !(req.body.buzzword === undefined || req.body.points === undefined);
+  if (findBuzzword > -1 && hasKeys) {
     buzzwords[findBuzzword].points = req.body.points;
     res.send(`{ "success": true }`);
   } else {
     res.send(`{ "success": false }`);
   }
-  console.log(buzzwords);
 });
 
 app.delete('/buzzwords', urlParser, (req, res) => {
@@ -47,15 +51,22 @@ app.delete('/buzzwords', urlParser, (req, res) => {
   } else {
     res.send(`{ "success": false }`);
   }
-  console.log(buzzwords);
 });
 
 app.post('/reset', urlParser, (req, res) => {
-  console.log(buzzwords);
+  buzzwords = [];
+  score = 0;
+  res.send(`{ "success": true }`);
 });
 
 app.post('/heard', urlParser, (req, res) => {
-  console.log(buzzwords);
+  const findBuzzword = buzzwords.indexOf(buzzwords.find((obj) => obj.buzzword === req.body.buzzword));
+  if (findBuzzword > -1) {
+    score += Number(buzzwords[findBuzzword].points);
+    res.send(`{ "totalScore": ${score} }`);
+  } else {
+    res.send(`{ "success": false }`);
+  }
 });
 
 const server = app.listen(PORT, () => {
